4eb61fee25d3ba1e77cd37aeb64f379d932d5a36
Sanjay Saini <Sanjay Saini@SanjaySaini>
2018-07-16 11:31:23 +0530

    mail detail logs

--- src/handle_incoming_email.py
+++ src/handle_incoming_email.py
@@ -13,7 +13,13 @@ import webapp2
 class LogSenderHandler(InboundMailHandler):
     def receive(self, mail_message):
         logging.info("Received a message from: " + mail_message.sender)
-        
-        
-        
+        sender = mail_message.sender
+        subject = mail_message.subject
+        date = mail_message.date
+        attachments = mail_message.attachments
+        logging.info('subject: %s' %(subject))
+        logging.info('date: %s, type: %s' %(date, type(date)))
+        logging.info(attachments)  
+        for atch in attachments: 
+          logging.info('atch: %s, type: %s' %(atch, type(atch)))
 app = webapp2.WSGIApplication([LogSenderHandler.mapping()], debug=True)
